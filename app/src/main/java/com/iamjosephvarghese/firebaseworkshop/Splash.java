13845f01b8176eae8b4d4272ba9d71c69e945f2e
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-11-14 02:04:10 +0530

    :sparkles: Created splash page with handler

---
+++ app/src/main/java/com/iamjosephvarghese/firebaseworkshop/Splash.java
@@ -0,0 +1,29 @@
+package com.iamjosephvarghese.firebaseworkshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent homeIntent = new Intent(Splash.this,MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+
+            }
+        },2000);
+
+    }
+}
