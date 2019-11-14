1f8c6590cab848d601633122ce001c8666547b99
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-11-14 16:34:39 +0530

    :sparkles: Starting quote fetching

---
+++ app/src/main/java/com/iamjosephvarghese/firebaseworkshop/ListQuotes.java
@@ -0,0 +1,14 @@
+package com.iamjosephvarghese.firebaseworkshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListQuotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_quotes);
+    }
+}
