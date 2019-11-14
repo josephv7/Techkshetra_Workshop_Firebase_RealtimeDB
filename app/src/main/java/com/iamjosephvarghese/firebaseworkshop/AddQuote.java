f0f6e090a8bc629b3aa62cea97e483b8c57ee41a
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-11-14 15:36:56 +0530

    Adding quote from edittext

--- app/src/main/java/com/iamjosephvarghese/firebaseworkshop/AddQuote.java
+++ app/src/main/java/com/iamjosephvarghese/firebaseworkshop/AddQuote.java
@@ -4,6 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -14,14 +17,22 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class AddQuote extends AppCompatActivity {
 
+    Button button;
+    EditText textBox;
+    EditText authorBox;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_quote);
 
 
+        textBox = findViewById(R.id.textBox);
+        authorBox = findViewById(R.id.authorBox);
+
+
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference ref = database.getReference("quotes");
+        final DatabaseReference ref = database.getReference("quotes");
 
 //        TODO Step 1
 
@@ -36,21 +47,51 @@ public class AddQuote extends AppCompatActivity {
         ref.child("data3").child("1").setValue(quote);
 
 //        TODO Step4
-        String pushId = ref.push().getKey();
+        final String pushId = ref.push().getKey();
 //        ref.child("data4").child(pushId).setValue(new Quote("Second Quote","jv"));
 
 //        TODO Step 5
-        ref.child("data4").child(pushId).setValue(quote).addOnCompleteListener(new OnCompleteListener<Void>() {
+//        ref.child("data4").child(pushId).setValue(quote).addOnCompleteListener(new OnCompleteListener<Void>() {
+//            @Override
+//            public void onComplete(@NonNull Task<Void> task) {
+//                Toast.makeText(AddQuote.this, "Success!", Toast.LENGTH_SHORT).show();
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Toast.makeText(AddQuote.this, "Task Failed!", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+//        TODO Step 6
+        Button button;
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                Toast.makeText(AddQuote.this, "Success!", Toast.LENGTH_SHORT).show();
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(AddQuote.this, "Task Failed!", Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                if(authorBox.getText().toString() != null && textBox.getText().toString() != null) {
+
+
+                    Quote quoteFromUser = new Quote(textBox.getText().toString(),authorBox.getText().toString());
+                    ref.child("data5").child(pushId).setValue(quoteFromUser).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Toast.makeText(AddQuote.this, "Quote Added", Toast.LENGTH_SHORT).show();
+                            authorBox.setText("");
+                            textBox.setText("");
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(AddQuote.this, "Error", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
             }
         });
+
 
     }
 }
