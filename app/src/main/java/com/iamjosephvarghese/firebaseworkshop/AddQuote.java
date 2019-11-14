ba9440592b330abaa2a377649676d152bfcfbc7a
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-11-14 14:39:46 +0530

    :sparkles: Pushing basic data

--- app/src/main/java/com/iamjosephvarghese/firebaseworkshop/AddQuote.java
+++ app/src/main/java/com/iamjosephvarghese/firebaseworkshop/AddQuote.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class AddQuote extends AppCompatActivity {
 
     @Override
@@ -12,6 +15,12 @@ public class AddQuote extends AppCompatActivity {
         setContentView(R.layout.activity_add_quote);
 
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("quotes");
+
+//        TODO Step 1
+
+        ref.child("myData").setValue("hi");
 
     }
 }
