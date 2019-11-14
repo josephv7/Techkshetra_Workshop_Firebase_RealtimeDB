bb37038e6c694a5f71058bb87e6f4a2e95ace9be
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-11-14 14:24:30 +0530

    :sparkles: Adding activity for uploading quites

---
+++ app/src/main/java/com/iamjosephvarghese/firebaseworkshop/AddQuote.java
@@ -0,0 +1,17 @@
+package com.iamjosephvarghese.firebaseworkshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddQuote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_quote);
+
+
+
+    }
+}
