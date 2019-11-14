3cb03fc2bda574c0b4456733aca01428235f215b
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-11-14 15:11:55 +0530

    adding listeners

--- app/src/main/java/com/iamjosephvarghese/firebaseworkshop/AddQuote.java
+++ app/src/main/java/com/iamjosephvarghese/firebaseworkshop/AddQuote.java
@@ -1,9 +1,14 @@
 package com.iamjosephvarghese.firebaseworkshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -20,10 +25,32 @@ public class AddQuote extends AppCompatActivity {
 
 //        TODO Step 1
 
-//        ref.child("myData").setValue("hi");
+//        ref.child("data1").setValue("hi");
 
 //        TODO Step 2
-        ref.child("myData").child("1").setValue("new data");
+//        ref.child("data2").child("1").setValue("new data");
+
+//        TODO Step 3
+
+        Quote quote = new Quote("This is a quote","iamjosephvarghese");
+        ref.child("data3").child("1").setValue(quote);
+
+//        TODO Step4
+        String pushId = ref.push().getKey();
+//        ref.child("data4").child(pushId).setValue(new Quote("Second Quote","jv"));
+
+//        TODO Step 5
+        ref.child("data4").child(pushId).setValue(quote).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Toast.makeText(AddQuote.this, "Success!", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(AddQuote.this, "Task Failed!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
